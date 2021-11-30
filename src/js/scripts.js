660fd98db544573acdd4ff455df048f502985633
sleepmanserg <sleepmanserg@gmail.com>
2021-11-30 22:15:41 +0200

    Dealer cabinet dropdown added

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -11,6 +11,18 @@ $(document).ready(function () {
 				theme.productsSlider();
 				theme.stepsSlider();
 				theme.scrollAnim();
+				theme.passwordShow();
+			},
+
+			passwordShow: () => {
+				const togglePassword = document.querySelector('#togglePassword');
+				const password = document.querySelector('#id_password');
+
+				togglePassword.addEventListener('click', function (e) {
+					const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+					password.setAttribute('type', type);
+					this.classList.toggle('active');
+				});
 			},
 
 			scrollAnim: () => {
